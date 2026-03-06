c02367cbc26914abb6bc7db6dcb32ee22a48c8b6
i567283 <radoslav.dodnikov@sap.com>
2026-03-06 16:30:33 +0200

    Fix URL in Astro config to generate correct sitemap links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  site: "https://whatdotracterssee.com",
+  site: "https://metapreview.net",
   adapter: node({
     mode: "standalone",
   }),
