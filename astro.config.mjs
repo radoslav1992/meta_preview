280ef9f9a9d59ee7ef6303e01024ab622f53d75c
i567283 <radoslav.dodnikov@sap.com>
2026-03-06 15:37:03 +0200

    Initial commit of What Do Trackers See Privacy App

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  site: "https://whatdotracterssee.com",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [sitemap()],
+});
